a3cc03dd4eac1423d55fcd1b10e52178f6e960ad
jafarimahdi <mahdijafarii.mj@gmail.com>
2019-03-07 10:34:16 +0100

    --

---
+++ game/subint.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+//  Create a function that takes a number and an array of numbers as a parameter
+//  Returns the indices of the numbers in the array of which the first number is a part of
+//  Or returns an empty list if the number is not part of any of the numbers in the array
+
+function subint(num,list) {
+  let newlist;
+  list.forEach(element => {
+    if(element.includes(1))
+    newlist.push(element)
+    
+  }); 
+
+  
+  
+
+  return newlist
+}
+
+
+
+
+
+
+// Example
+console.log(subint(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+console.log(subint(9, [1, 11, 34, 52, 61]));
+// should print: '[]'
